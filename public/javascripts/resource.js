065e6b2bb107152353af44169a865ee9ed9c319f
iteacat <justwy@gmail.com>
2015-04-15 08:47:58 -0400

    add form validation and improve UI

--- public/javascripts/resource.js
+++ public/javascripts/resource.js
@@ -10,22 +10,23 @@ app.controller("MainCtrl", function(initMap) {
 app.controller("ChatCtrl", ['$scope', 'chatRsc', function($scope, chatRsc) {
     var socket = io();
     $scope.msg = '';
+    $scope.userName = 'anonymous';
     chatRsc.get(function(data) {
         $scope.chats = data.chats;
     });
     $scope.CHAT_LEN = 100;
 
-    socket.on('new msg', function (text) {
-        $scope.chats.unshift(text);
+    socket.on('new msg', function (data) {
+        $scope.chats.unshift(data);
         while ($scope.CHAT_LEN < $scope.chats.length) {
             $scope.chats.pop();
         }
         $scope.$apply();
-        console.log('received new msg: ', $scope.chats);
+        console.log('received new msg: ', data);
     });
     $scope.submit = function () {
         console.log('sending msg: ', $scope.msg);
-        socket.emit('new msg', $scope.msg);
+        socket.emit('new msg', {msg: $scope.msg, id: $scope.userName});
         $scope.msg = '';
     };
 }]);
