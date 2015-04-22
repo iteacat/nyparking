a3629817eebdaea3378ad43b224cc2b671ed644b
iteacat <justwy@gmail.com>
2015-04-21 23:19:44 -0400

    change to use connection pool. Costmatic UI change

---
+++ dao/nypCommon.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * Created by yin on 4/21/15.
+ */
+
+var mysql = require('mysql');
+
+exports.dbConfig = {
+    host: 'cat1ny.ch67iaxvc4uj.us-east-1.rds.amazonaws.com',
+    user: 'iteacat',
+    password: 'ChafNod9',
+    port: '3306',
+    database: 'cat1ny',
+    connectionLimit : 50,
+    multipleStatements : true
+};
+
+exports.dbConfigDev = {
+    host: '192.168.56.101',
+    user: 'root',
+    password: null,
+    port: '3306',
+    database: 'cat1ny',
+    connectionLimit : 50,
+    multipleStatements : true
+};
+
+var pool  = mysql.createPool(
+    process.argv[2] === 'prod' ? this.dbConfig : this.dbConfigDev
+);
+
+exports.getConnection = function(callback) {
+    pool.getConnection(function(err, connection) {
+        if (err)
+            console.error('CAO! Error on getting connection. ');
+        callback(err, connection);
+    });
+}
--- dao/sign_dao.js
+++ dao/sign_dao.js
@@ -3,63 +3,42 @@
  */
 
 var mysql = require('mysql');
+var nypCommon = require('./nypCommon.js');
 
-var radius = 0.001;
 var sql = "set @center=point(?, ?);" +
-" SET @radius = ?; " +
-" SET @bbox = CONCAT('POLYGON((', " +
-" X(@center) - @radius, ' ', Y(@center) - @radius, ',', " +
-" X(@center) + @radius, ' ', Y(@center) - @radius, ',', " +
-" X(@center) + @radius, ' ', Y(@center) + @radius, ',', " +
-" X(@center) - @radius, ' ', Y(@center) + @radius, ',', " +
-" X(@center) - @radius, ' ', Y(@center) - @radius, '))' " +
-" ); " +
-" SELECT " +
+    " SET @radius = ?; " +
+    " SET @bbox = CONCAT('POLYGON((', " +
+    " X(@center) - @radius, ' ', Y(@center) - @radius, ',', " +
+    " X(@center) + @radius, ' ', Y(@center) - @radius, ',', " +
+    " X(@center) + @radius, ' ', Y(@center) + @radius, ',', " +
+    " X(@center) - @radius, ' ', Y(@center) + @radius, ',', " +
+    " X(@center) - @radius, ' ', Y(@center) - @radius, '))' " +
+    " ); " +
+    " SELECT " +
     "X(location) as x, Y(location) as y, boro, sign_desc, " +
     "SQRT(POW( ABS( X(location) - X(@center)), 2) + POW( ABS(Y(location) - Y(@center)), 2 )) AS distance " +
-" FROM nyparking_signs " +
-" WHERE Intersects(location, GeomFromText(@bbox) ) " +
-" AND SQRT(POW( ABS( X(location) - X(@center)), 2) + POW( ABS(Y(location) - Y(@center)), 2 )) < @radius " +
-" ORDER BY distance";
-
-var connection  = mysql.createConnection({
-    host     : 'cat1ny.ch67iaxvc4uj.us-east-1.rds.amazonaws.com',
-    user     : 'iteacat',
-    password : 'ChafNod9',
-    port     : '3306',
-    database : 'cat1ny',
-    multipleStatements : true
-});
+    " FROM nyparking_signs " +
+    " WHERE Intersects(location, GeomFromText(@bbox) ) " +
+    " AND SQRT(POW( ABS( X(location) - X(@center)), 2) + POW( ABS(Y(location) - Y(@center)), 2 )) < @radius";
 
 function getSigns(x, y, radius, callback) {
-    //connection.connect(function(err) {
-        //if (err) {
-            //console.error('error connecting: ' + err.stack);
-            //return;
-        //}
-//
-        //console.log('connected as id ' + connection.threadId);
-    //});
+    nypCommon.getConnection(function (err, conn) {
+        conn.query(sql, [x, y, radius], function (err, rows, fields) {
+            conn.release();
+            if (err && err.errno !== 1062) {
 
-    //console.log(sql);
+                console.error('error writing to db ', err);
+            }
 
-    connection.query(sql, [x, y, radius], function(err, rows, fields) {
-        if (err && err.errno !== 1062) {
-
-            console.error('error writing to db ', err);
-        }
-
-        //console.log("rows ", rows.splice(3)[0]);
-        var ret;
-        if (rows === undefined || rows.length === 0)
-            ret = [];
-        else
-            ret = rows.splice(3)[0];
-        callback(ret);
+            //console.log("rows ", rows.splice(3)[0]);
+            var ret;
+            if (rows === undefined || rows.length === 0)
+                ret = [];
+            else
+                ret = rows.splice(3)[0];
+            callback(ret);
+        });
     });
-
 }
-
-//getSigns(40.740352, -73.8861533, radius);
 
 module.exports = getSigns;
